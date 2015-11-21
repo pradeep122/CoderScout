898a89a9d3c57747571e2c0324a158677aa7430b
Rohit <rohit@yantranet.com>
2015-11-21 21:31:42 +0530

    integrated beginTest Module
    Signed-off-by: Rohit <rohit5ram@gmail.com>

--- client/app/components/welcome/welcomeCtrl.js
+++ client/app/components/welcome/welcomeCtrl.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('coderScout')
-    .controller('welcomeCtrl', function($scope) {
-        $scope.message = "hello world";
+    .controller('welcomeCtrl', function($scope, apiRegistry, $stateParams) {
+        $scope.validateEmail = function() {
+            var emailRegEx = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+            $scope.errorMsg = !emailRegEx.test($scope.userEmail);
+        };
+
+        $scope.beginTestReq = function() {
+            apiRegistry.beginTest($scope.userEmail, $stateParams.access_key).then(function() {
+
+            });
+        };
     });
